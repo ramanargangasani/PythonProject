485393fbdc4aa0b0b2d1995d1cb258f4c38ca337
Ramana <Ramana@gmail.com>
2020-06-18 02:29:55 -0500

    updated 3rd method by ramanag

--- TestData/ExcelDataDemo.py
+++ TestData/ExcelDataDemo.py
@@ -21,7 +21,7 @@ print(sheet['A3'].value)
 for i in range(1, sheet.max_row + 1):
     if sheet.cell(row=i, column=1).value == "Testcase2":
         for j in range(2, sheet.max_column + 1):
-            #
+            # assigning values to dictionary
             Dict[sheet.cell(row=1,column=j).value] = sheet.cell(row=i, column=j).value
 print(Dict)
 
--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -27,6 +27,18 @@ class HomePageData:
                     Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
         return [Dict]
 
+    @staticmethod  # if decalare staticmethod there is no self. if self required you need to declare a object
+    def getExcelTestData3(test_case_name):  # this method for testing git hub should be removed after that
+        Dict = {}
+        book = openpyxl.load_workbook("C:\\Users\\HarshaLalith\\Desktop\\Python Info\\exceltestdata.xlsx")
+        sheet = book.active
+        for i in range(1, sheet.max_row + 1):
+            if sheet.cell(row=i, column=1).value == test_case_name:  # test_case_name = Testcase2
+                for j in range(2, sheet.max_column + 1):
+                    # Dict['lastname'] ="Ramana"
+                    Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+        return [Dict]
+
 
 
 
